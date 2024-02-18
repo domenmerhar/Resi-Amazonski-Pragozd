2d655586cdf7ef038753c7e764863d8856c10605
Domen Merhar <domen.merhar@students.vegova.si>
2024-02-18 11:21:21 +0100

    Added Spawn Locations

--- SDL igra/Source.cpp
+++ SDL igra/Source.cpp
@@ -1,5 +1,7 @@
 #include <SDL.h>
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
@@ -38,14 +40,44 @@ public:
 class GameObject {
 protected:
 	int x, y, movementSpeed, width, height;
+	bool visible;
 
 	SDL_Texture* texture;
 	SDL_Rect sourceRectangle, destinationRectangle;
 	SDL_Renderer* renderer;
 	SDL_Surface* tempSurface;
 
+	void UpdateSourceRectangle() {
+		sourceRectangle.h = height;
+		sourceRectangle.w = width;
+		sourceRectangle.x = 0;
+		sourceRectangle.y = 0;
+	}
+
+	void UpdateDestinationRectangle() {
+		if (Util::WithinBoundsX(x, width)) destinationRectangle.x = x;
+		else
+			x = destinationRectangle.x = Util::ResetX(x, width);
+
+		if (Util::WithinBoundsY(y, height * 2)) destinationRectangle.y = y;
+		else y = destinationRectangle.y = Util::ResetY(y, height * 2);
+
+		destinationRectangle.w = sourceRectangle.w * 2;
+		destinationRectangle.h = sourceRectangle.h * 2;
+	}
 public:
-	GameObject(int red, int green, int blue, SDL_Renderer* renderer, int x, int y, int movementSpeed, int width, int height) {
+	void Update() {
+		if (visible) {
+			UpdateSourceRectangle();
+			UpdateDestinationRectangle();
+		}
+	}
+
+	void Render() {
+        if(visible) SDL_RenderCopy(renderer, texture, &sourceRectangle, &destinationRectangle);
+	}
+
+	void Init(int red, int green, int blue, SDL_Renderer* renderer, int x, int y, int movementSpeed, int width, int height, bool visible) {
 		this->width = width;
 		this->height = height;
 
@@ -61,38 +93,45 @@ public:
 		this->movementSpeed = movementSpeed;
 
 		UpdateSourceRectangle();
+		this->visible = visible;
 	}
 
-	void UpdateSourceRectangle() {
-		sourceRectangle.h = height;
-		sourceRectangle.w = width;
-		sourceRectangle.x = 0;
-		sourceRectangle.y = 0;
+	GameObject(int red, int green, int blue, SDL_Renderer* renderer, int x, int y, int movementSpeed, int width, int height, bool visible) {
+		Init(red, green, blue, renderer, x, y, movementSpeed, width, height, visible);
 	}
 
-	void Update() {
-		/*x += movementSpeed;
-		y += movementSpeed;*/
-
-		UpdateSourceRectangle();
-
-		if (Util::WithinBoundsX(x, width)) destinationRectangle.x = x;
-		else 
-			x = destinationRectangle.x = Util::ResetX(x, width);
-
-		if(Util::WithinBoundsY(y, height * 2)) destinationRectangle.y = y;
-		else y = destinationRectangle.y = Util::ResetY(y, height * 2);
-
-		//cout << "x: " << x << " y: " << y << endl;
-
-		destinationRectangle.w = sourceRectangle.w * 2;
-		destinationRectangle.h = sourceRectangle.h * 2;
+	void Hide() {
+		visible = false;
 	}
 
-	void Render() {
-        SDL_RenderCopy(renderer, texture, &sourceRectangle, &destinationRectangle);
+	void Show() {
+		visible = true;
+	}	
+
+	bool GetIsVisible() {
+		return visible;
 	}
 
+	int GetX() {
+		return x;
+	}
+
+	int GetY() {
+		return y;
+	}
+
+	void SetPosition(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	int getHeight() {
+		return height;
+	}
+
+	int getWidth() {
+		return width;
+	}
 };
 
 class Player : public GameObject {
@@ -100,51 +139,56 @@ public:
 	using GameObject::GameObject;
 
 	void HandleInput() {
-		const float diagonalSpeedMultiplier = 0.7071; // Approximately 1 / sqrt(2)
+		if (visible) {
+			const float diagonalSpeedMultiplier = 0.7071;
 
-		SDL_PumpEvents(); // Update the internal key state
+			SDL_PumpEvents();
 
-		const Uint8* keys = SDL_GetKeyboardState(NULL);
+			const Uint8* keys = SDL_GetKeyboardState(NULL);
 
-		float dx = 0, dy = 0;
+			float dx = 0, dy = 0;
 
-		// Determine the movement direction based on key states
-		if (keys[SDL_SCANCODE_UP]) {
-			dy -= 1;
+			if (keys[SDL_SCANCODE_UP]) {
+				dy -= 1;
+			}
+			if (keys[SDL_SCANCODE_DOWN]) {
+				dy += 1;
+			}
+			if (keys[SDL_SCANCODE_LEFT]) {
+				dx -= 1;
+			}
+			if (keys[SDL_SCANCODE_RIGHT]) {
+				dx += 1;
+			}
+
+			if (dx != 0 && dy != 0) {
+				dx = dx * diagonalSpeedMultiplier;
+				dy = dy * diagonalSpeedMultiplier;
+			}
+
+			x += dx * movementSpeed;
+			y += dy * movementSpeed;
 		}
-		if (keys[SDL_SCANCODE_DOWN]) {
-			dy += 1;
-		}
-		if (keys[SDL_SCANCODE_LEFT]) {
-			dx -= 1;
-		}
-		if (keys[SDL_SCANCODE_RIGHT]) {
-			dx += 1;
-		}
-
-		// If diagonal movement detected, adjust dx and dy
-		if (dx != 0 && dy != 0) {
-			dx = dx * diagonalSpeedMultiplier;
-			dy = dy * diagonalSpeedMultiplier;
-		}
-
-		// Update the position
-		x +=  dx * movementSpeed;
-		y += dy * movementSpeed;
 	}
 };
 
-
 Player* player;
+vector<GameObject*> playerSpawnSquares(5);
 
 class Game
 {
 	bool isRunning = false;
 	SDL_Window* window = NULL;
-	SDL_Renderer* gameRenderer;
+	SDL_Renderer* gameRenderer = NULL;
+
+	void HideSpawnSquares() {
+		for (int i = 0; i < playerSpawnSquares.size(); i++) {
+			playerSpawnSquares[i]->Hide();
+		}
+	}
 		
 public:
-	Game(const char* title, int x, int y, int width, int height, bool fullscreen) {
+	void Init(const char* title, int x, int y, int width, int height, bool fullscreen) {
 		int flags = 0;
 
 		if (fullscreen) {
@@ -170,30 +214,61 @@ public:
 			isRunning = true;
 		}
 
-		player = new Player(255, 255, 255, gameRenderer, 0, 0, 5, 32, 32);
+		player = new Player(255, 255, 255, gameRenderer, 0, 0, 5, 32, 32, true);
+		playerSpawnSquares[0] = new GameObject(200, 200, 200, gameRenderer, 0, 0, 0, 64, 64, true);
+		playerSpawnSquares[1] = new GameObject(200, 200, 200, gameRenderer, Util::windowWidth - 64, 0, 0, 64, 64, true);
+		playerSpawnSquares[2] = new GameObject(200, 200, 200, gameRenderer, 0, Util::windowHeight - 64, 0, 64, 64, true);
+		playerSpawnSquares[3] = new GameObject(200, 200, 200, gameRenderer, Util::windowWidth - 64, Util::windowHeight - 64, 0, 64, 64, true);
+		playerSpawnSquares[4] = new GameObject(200, 200, 200, gameRenderer, Util::windowWidth / 2 - 64, Util::windowHeight  / 2- 64, 0, 64, 64, true);
 	}
 
 	void HandleEvents() {
 		SDL_Event event;
-		SDL_PollEvent(&event);
 
-		switch (event.type) {
-			case SDL_QUIT:
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
 				isRunning = false;
-				break;
+			}
+			else if (event.type == SDL_MOUSEBUTTONDOWN) {
+				if (event.button.button == SDL_BUTTON_LEFT) {
+					int x = event.button.x;
+					int y = event.button.y;
 
-			default:
-				break;
+					for (int i = 0; i < playerSpawnSquares.size(); i++) {
+						if (
+							playerSpawnSquares[i]->GetX() - 100 <= x && x <= playerSpawnSquares[i]->GetX() + 100 &&
+							playerSpawnSquares[i]->GetY() - 100 <= y && y <= playerSpawnSquares[i]->GetY() + 100
+							) {
+
+							player->SetPosition(
+								playerSpawnSquares[i]->GetX() + playerSpawnSquares[i]->getHeight() / 2,
+								playerSpawnSquares[i]->GetY() + playerSpawnSquares[i]->getHeight() / 2
+							);
+							HideSpawnSquares();
+							break;
+						}
+					}
+				}
+			}
 		}
 	};
 
 	void Update() {
 		player->Update();
+
+		for (int i = 0; i < playerSpawnSquares.size(); i++) {
+			playerSpawnSquares[i]->Update();
+		}
 	};
 
 	void Render() {
 		SDL_RenderClear(gameRenderer);
+
 		player->Render();
+		for (int i = 0; i < playerSpawnSquares.size(); i++) {
+			playerSpawnSquares[i]->Render();
+		}
+
 		SDL_RenderPresent(gameRenderer);
 	};
 
@@ -244,7 +319,8 @@ int main(int argc, char* argv[]) {
 
 	FrameManager frameManager;
 
-	Game *game = new Game("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Util::windowWidth, Util::windowHeight, isFullscreen);
+	Game *game = new Game();
+	game->Init("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Util::windowWidth, Util::windowHeight, isFullscreen);	
 
 	while (game->IsRunning()) {
 
