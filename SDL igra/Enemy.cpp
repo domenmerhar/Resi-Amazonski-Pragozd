274500cca65d8b4791a0a99be421bf97faacfc98
Domen Merhar <domen.merhar@students.vegova.si>
2024-02-29 15:52:08 +0100

    Exported Enemy class

---
+++ SDL igra/Enemy.cpp
@@ -0,0 +1,161 @@
+#include "Enemy.h"
+#include "Util.h"
+#include "Forest.h"
+
+#include <cmath>
+#include <SDL.h>
+
+
+void Enemy::GenerateRandomTarget() {
+    targetX = Util::GetRandomX(width);
+    targetY = Util::GetRandomY(height);
+
+    targetRow = targetY / 64;
+    targetColumn = targetX / 64;
+}
+
+Enemy::Enemy(SDL_Renderer* renderer, int movementSpeed, int width, int height, Forest* forest,
+    const char* pathImageSmallLeft, const char* pathImageSmallRight, const char* pathImageBigRight, const char* pathImageBigLeft) {
+    this->width = width;
+    this->height = height;
+
+    surfaceSmallLeft = IMG_Load(pathImageSmallLeft);
+    surfaceSmallRight = IMG_Load(pathImageSmallRight);
+
+    surfaceBigLeft = IMG_Load(pathImageBigLeft);
+    surfaceBigRight = IMG_Load(pathImageBigRight);
+
+    this->renderer = renderer;
+
+    SDL_Surface* tempSurface = IMG_Load(pathImageSmallRight);
+    this->texture = SDL_CreateTextureFromSurface(this->renderer, tempSurface);
+    SDL_FreeSurface(tempSurface);
+
+    this->forest = forest;
+
+    Reset(movementSpeed);
+}
+
+void Enemy::Reset(int movementSpeed) {
+    spawnX = x = Util::GetRandomX(width);
+    spawnY = y = Util::GetRandomY(height);
+    this->movementSpeed = movementSpeed;
+
+    UpdateSourceRectangle();
+
+    GenerateRandomTarget();
+
+    SetBig(false);
+    visible = true;
+
+    boundingBox = GetBoundingBox();
+}
+
+void Enemy::Move() {
+    float dx = targetX - x;
+    float dy = targetY - y;
+
+    float magnitude = sqrt(dx * dx + dy * dy);
+
+    if (!forest->CanBeDestroyed(targetRow, targetColumn))
+        GenerateRandomTarget();
+    else {
+        if (magnitude > 0) {
+            dx /= magnitude;
+            dy /= magnitude;
+
+            x += round(dx * movementSpeed);
+            y += round(dy * movementSpeed);
+        }
+        else {
+            forest->StartBurning(targetRow, targetColumn);
+            GenerateRandomTarget();
+        }
+
+        if (dx > 0 && !isBig) {
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceSmallRight);
+        }
+        else if (dx < 0 && !isBig) {
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceSmallLeft);
+        }
+        else if (dx < 0 && isBig) {
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceBigRight);
+        }
+        else if (dx > 0 && isBig) {
+            texture = SDL_CreateTextureFromSurface(renderer, surfaceBigLeft);
+        }
+    }
+}
+
+void Enemy::Hide() {
+    visible = false;
+    x = spawnX;
+    y = spawnY;
+}
+
+void Enemy::SetTarget(Tree* tree) {
+    targetX = tree->GetX() + tree->getWidth() / 2;
+    targetY = tree->GetY() + tree->getHeight() / 2;
+}
+
+void Enemy::SetBig(bool toSet) {
+    if (isBig == toSet) return;
+
+    isBig = toSet;
+}
+
+bool Enemy::GetIsBig() {
+    return isBig;
+}
+
+void Enemy::HandleCollision(std::vector<Ally*> allies, std::vector<Enemy*> enemies, Player* player) {
+    if (!visible) return;
+
+    boundingBox = GetBoundingBox();
+    SDL_Rect playerBoundingBox = player->GetBoundingBox();
+    bool invinciblePlayer = false;
+
+    for (Ally* ally : allies) {
+        bool playerIsCloseToAlly = false;
+        if (ally != nullptr && ally->GetIsVisible()) {
+            SDL_Rect allyBoundingBox = ally->GetBoundingBox();
+
+            if ((abs(playerBoundingBox.x - allyBoundingBox.x) < 100 && abs(playerBoundingBox.y - allyBoundingBox.y) < 100) && !playerIsCloseToAlly) {
+                playerIsCloseToAlly = true;
+                invinciblePlayer = true;
+            }
+
+            if (SDL_HasIntersection(&allyBoundingBox, &boundingBox)) {
+                if (!isBig) Hide();
+                else if (isBig) {
+                    if (SDL_HasIntersection(&playerBoundingBox, &boundingBox)) {
+                        this->Hide();
+                    }
+                    else if (!playerIsCloseToAlly) {
+                        ally->Hide();
+                    }
+                }
+            }
+        }
+    }
+
+    for (Enemy* enemy : enemies) {
+        if (enemy != nullptr && enemy->GetIsVisible() && enemy != this) {
+            SDL_Rect enemyBoundingBox = enemy->GetBoundingBox();
+            if (SDL_HasIntersection(&enemyBoundingBox, &boundingBox) && isBig == false) {
+                enemy->Hide();
+                SetBig(true);
+            }
+        }
+    }
+
+    if (!isBig && SDL_HasIntersection(&playerBoundingBox, &boundingBox)) {
+        this->Hide();
+    }
+
+    if (!player->GetIsVisible() || invinciblePlayer) return;
+
+    if (isBig && SDL_HasIntersection(&playerBoundingBox, &boundingBox)) {
+        player->Hide();
+    }
+}
--- SDL igra/Enemy.h
+++ SDL igra/Enemy.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include "GameObject.h"
+#include "Forest.h"
+#include "Ally.h"
+#include "Player.h"
+#include <vector>
+
+class Enemy : public GameObject {
+    int spawnX, spawnY;
+    int targetX, targetY;
+
+    int targetRow, targetColumn;
+
+    bool isBig;
+
+    Forest* forest;
+
+    SDL_Rect boundingBox;
+
+    SDL_Surface* surfaceSmallLeft, * surfaceSmallRight, * surfaceBigLeft, * surfaceBigRight;
+
+    void GenerateRandomTarget();
+
+public:
+    Enemy(SDL_Renderer* renderer, int movementSpeed, int width, int height, Forest* forest,
+        const char* pathImageSmallLeft, const char* pathImageSmallRight, const char* pathImageBigRight, const char* pathImageBigLeft);
+
+    void Reset(int movementSpeed);
+
+    void Move();
+
+    void Hide();
+
+    void SetTarget(Tree* tree);
+
+    void SetBig(bool toSet);
+
+    bool GetIsBig();
+
+    void HandleCollision(std::vector<Ally*> allies, std::vector<Enemy*> enemies, Player* player);
+};
